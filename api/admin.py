7260fce0e816f0dcf7766ef9051e5a4658800fb7
Emeka Anyaegbunam <emekaanyaegbunam@Emekas-MacBook-Pro.local>
2024-08-08 16:54:05 +0100

    updated

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import CustomUser, SavingPlan, WeeklyAmount
 
+class SavingPlanInline(admin.TabularInline):
+    model = SavingPlan
+    extra = 1
+
+class CustomUserAdmin(admin.ModelAdmin):
+    inlines = [SavingPlanInline]
+
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(SavingPlan)
 admin.site.register(WeeklyAmount)
+
